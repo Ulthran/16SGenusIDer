e0dfed3b8e0b2bcd54018a967061e84ecccb52bf
ctbus <ctbushman@gmail.com>
2023-02-24 10:51:30 -0500

    Playing with sys.path now

--- tests/test_CLI.py
+++ tests/test_CLI.py
@@ -1,4 +1,11 @@
+import os
 import pytest
+import sys
+from pathlib import Path
+
+print(sys.path)
+sys.path.append(Path(os.path.dirname(__file__)).parent)
+print(sys.path)
 from src.GenusFinder.CLI import CLI, MuscleAligner, RAxMLTreeBuilder
 
 
@@ -14,5 +21,6 @@ def raxml_fixture():
 
 def test_muscle_call(muscle_fixture):
     cli: MuscleAligner = muscle_fixture
-
+    print(f"PYTHONPATH: {os.environ.get('PYTHONPATH')}")
+    print(f"sys.path: {sys.path}")
     cli.call()
