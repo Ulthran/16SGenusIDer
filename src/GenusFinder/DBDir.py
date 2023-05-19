53c2c07c7afbc3bb64e1ef8451adf8889e03a4ac
ctbus <ctbushman@gmail.com>
2023-05-18 23:51:32 -0400

    Finish DBDir updates?

--- src/GenusFinder/DBDir.py
+++ src/GenusFinder/DBDir.py
@@ -30,31 +30,135 @@ class DBFile:
         return self.fp
 
 
-class LTPAlignment(DBFile):
+class LTPFile(DBFile):
+    """
+    Base class to define an LTP file in the database and associated operations
+    """
+
+    def __init__(self, fp: Path) -> None:
+        super().__init__(fp)
+
+        self.LTP_URL = "https://imedea.uib-csic.es/mmg/ltp/wp-content/uploads/ltp/"
+
+    def get(self) -> Path:
+        if not self.fp.exists():
+            url = self.url_for(self.fn)
+            logging.info(f"Fetching {url}...")
+            with urlopen(url) as resp, open(self.fp, "wb") as f:
+                shutil.copyfileobj(resp, f)
+        else:
+            logging.info(f"Found {self.fp}, skipping download...")
+
+        return self.fp
+
+    def url_for(self, name: str) -> str:
+        return f"{self.LTP_URL}{name}"
+
+
+class LTPAlignment(LTPFile):
     """
     Holds and operates on the LTP alignment
     """
 
     def __init__(self, fp: Path) -> None:
         super().__init__(fp)
+    
+    def get(self) -> Path:
+        if self.fp.exists():
+            return super().get()
+        else:
+            ret = super().get()
+            self.clean()
+            return ret
+    
+    def clean(self):
+        replacements_map = {
+            " ": "", # LTP's fun syntax
+            "\n": "",
+            ".": "-",
+            "U": "T",
+            "R": "A", # Choose a letter per abbreviation and stick with it
+            "Y": "T",
+            "M": "C",
+            "K": "G",
+            "S": "C",
+            "W": "T",
+            "H": "A",
+            "B": "G",
+            "V": "C",
+            "D": "T",
+            "N": "A",
+            "A": "A", # Keep what's already good
+            "C": "C",
+            "G": "G",
+            "T": "T",
+            "-": "-",
+        }
+
+        logging.info("Cleaning LTP alignment...")
+        temp_fp = self.fp.parents[0] / "temp_alignment.fasta"
+        with open(self.fp, "rbU") as f:
+            num_lines = sum(1 for _ in f)
+        with open(temp_fp, "w") as f_temp, open(self.fp) as f_align:
+            with tqdm(total=num_lines) as pbar:
+                for header_str, seq_str in parse_fasta(f_align):
+                    pbar.update(1)
+                    f_temp.write(f">{header_str}")
+                    f_temp.write("".join([replacements_map[c] for c in seq_str]))
+                
+        os.remove(self.fp)
+        os.rename(temp_fp, self.fp)
 
 
-class LTPTree(DBFile):
+class LTPTree(LTPFile):
     """
     Holds and operates on the LTP tree
     """
     
     def __init__(self, fp: Path) -> None:
         super().__init__(fp)
+    
+    def get(self) -> Path:
+        if self.fp.exists():
+            return super().get()
+        else:
+            ret = super().get()
+            self.clean()
+            return ret
+    
+    def clean(self):
+        logging.info("Cleaning LTP tree...")
+        temp_fp = self.fp.parents[0] / "temp_tree.ntree"
+        with open(temp_fp, "w") as f_temp, open(self.fp) as f_tree:
+            header = False
+            for line in f_tree.readlines():
+                if line[0] == "[":
+                    header = True
+                if header:
+                    if line[0] == "]":
+                        header = False
+                elif "'" in line and "," in line:
+                    new_id = line[line.index("'")+1:line.index(",", line.index("'"))]
+                    f_temp.write(line[:line.index("'")] + new_id + line[line.index("'", line.index("'")+1)+1:])
+                elif "'" in line:
+                    f_temp.write(line.replace("'", "").replace(" ", ""))
+                else:
+                    f_temp.write(line)
+
+        os.remove(self.fp)
+        os.rename(temp_fp, self.fp)
 
 
-class LTPBlast(DBFile):
+class LTPBlast(LTPFile):
     """
     Holds and operates on the LTP blast db
     """
 
     def __init__(self, fp: Path) -> None:
         super().__init__(fp)
+    
+    def get(self) -> Path:
+        return super().get()
 
 
 class TypeSpecies(DBFile):
@@ -107,17 +211,14 @@ class DBDir:
         os.makedirs(self.root_fp, exist_ok=True)
 
         self.key = esearch_api_key
-
         self.LTP_VERSION = "06_2022"
-        self.LTP_URL = f"https://imedea.uib-csic.es/mmg/ltp/wp-content/uploads/ltp/"
 
         self._16S_db = self.root_fp / "16S.db"
-        self.LTP_aligned_fp = self.root_fp / f"LTP_{self.LTP_VERSION}_aligned.fasta"
-        self.LTP_blastdb_fp = self.root_fp / f"LTP_{self.LTP_VERSION}_blastdb.fasta"
-        self.LTP_tree_fp = self.root_fp / f"LTP_all_{self.LTP_VERSION}.ntree"
-        self.rooted_LTP_tree_fp = self.root_fp / "RAxML_bestTree.rooted"
+        self.LTP_aligned = LTPAlignment(self.root_fp / f"LTP_{self.LTP_VERSION}_aligned.fasta")
+        self.LTP_blastdb = LTPBlast(self.root_fp / f"LTP_{self.LTP_VERSION}_blastdb.fasta")
+        self.LTP_tree = LTPTree(self.root_fp / f"LTP_all_{self.LTP_VERSION}.ntree")
         self.LTP_csv_fp = self.root_fp / f"LTP_{self.LTP_VERSION}.csv"
-        self.type_species_fp = self.root_fp / "type_species.fasta"
+        self.type_species = TypeSpecies(self.root_fp / "type_species.fasta")
 
     def get_16S_db(self) -> Path:
         if not self._16S_db.exists():
@@ -127,76 +228,6 @@ class DBDir:
             logging.info(f"Found {self._16S_db}, skipping download...")
 
         return self._16S_db
-
-    def get_LTP_aligned(self) -> Path:
-        #ret = self._get_LTP(self.LTP_aligned_fp, self.LTP_aligned_fp.name)
-        shutil.copyfile(self.root_fp / "LTP", self.LTP_aligned_fp)
-        self.clean_alignment()
-        if not self.verify_alignment():
-            sys.exit()
-        ret = self.LTP_aligned_fp
-        return ret
-
-    def get_LTP_blastdb(self) -> Path:
-        return self._get_LTP(self.LTP_blastdb_fp, self.LTP_blastdb_fp.name)
-
-    def get_LTP_tree(self) -> Path:
-        ret = self._get_LTP(self.LTP_tree_fp, self.LTP_tree_fp.name)
-        #if not self.verify_tree():
-        #    sys.exit()
-        return ret
-    
-    def get_rooted_LTP_tree(self) -> Path:
-        return 
-
-    def get_LTP_csv(self) -> Path:
-        return self._get_LTP(self.LTP_csv_fp, self.LTP_csv_fp.name)
-
-    def get_type_species(self) -> Path:
-        if not self.type_species_fp.exists():
-            logging.info(f"Creating {self.type_species_fp}...")
-            self._generate_type_species()
-        else:
-            logging.info(f"Found {self.type_species_fp}, skipping creation...")
-
-        return self.type_species_fp
-    
-    def _generate_type_species(self):
-        accession_cts = collections.defaultdict(int)
-        with open(self.get_LTP_blastdb()) as f_in:
-            with open(self.type_species_fp, "w") as f_out:
-                for desc, seq in self._parse_fasta(f_in):
-                    accession, species_name = self._parse_desc(desc)
-                    if not accession or not species_name:
-                        continue
-                    # Some accessions refer to genomes with more than one 16S gene
-                    # So accessions can be legitiamtely repeated with distinct gene sequences
-                    accession_times_previously_seen = accession_cts[accession]
-                    accession_cts[accession] += 1
-                    if accession_times_previously_seen > 0:
-                        accession = "{0}_repeat{1}".format(
-                            accession, accession_times_previously_seen
-                        )
-                    f_out.write(">{0}\t{1}\n{2}\n".format(accession, species_name, seq.replace(" ", "").replace("U", "T")))
-
-    def _get_LTP(self, fp: Path, name: str) -> Path:
-        if not fp.exists():
-            url = self.url_for(name)
-            logging.info(f"Fetching {url}...")
-            with urlopen(url) as resp, open(fp, "wb") as f:
-                shutil.copyfileobj(resp, f)
-            
-            if "aligned" in name:
-                self.clean_alignment()
-            elif "tree" in name:
-                self.clean_tree()
-        else:
-            logging.info(f"Found {fp}, skipping download...")
-
-        return fp
-
-    def url_for(self, name: str) -> str:
-        return f"{self.LTP_URL}{name}"
 
     def _create_16S_db(self):
         def chunker(seq, size):
@@ -219,96 +250,6 @@ class DBDir:
                 for seq in egs:
                     db.write(f">{str(seq)[6:-1]} {seq.organism}\n")
                     db.write(f"{seq.sequence}\n")
-
-    def clean_alignment(self):
-        replacements_map = {
-            " ": "", # LTP's weird syntax
-            ".": "----",
-            "U": "T---",
-            "R": "AG--", # Expand these abbreviations
-            "Y": "TC--",
-            "M": "CA--",
-            "K": "TG--",
-            "S": "CG--",
-            "W": "TA--",
-            "H": "TCA-",
-            "B": "TCG-",
-            "V": "CAG-",
-            "D": "TAG-",
-            "N": "TCAG",
-            "A": "A---", # Keep what's already good
-            "C": "C---",
-            "G": "G---",
-            "T": "T---",
-            "-": "----",
-            "\n": "\n",
-        }
-
-        logging.info("Cleaning LTP alignment...")
-        temp_fp = self.root_fp / "temp_alignment.fasta"
-        with open(temp_fp, "w") as f_temp, open(self.LTP_aligned_fp) as f_align:
-            with tqdm(total=len(f_align.readlines())) as pbar:
-                f_align.seek(0)
-                for line in f_align.readlines():
-                    pbar.update(1)
-                    if line[0] == ">":
-                        f_temp.write(f"{line}")
-                    else:
-                        f_temp.write("".join([replacements_map[c] for c in line]))
-                
-        os.remove(self.LTP_aligned_fp)
-        os.rename(temp_fp, self.LTP_aligned_fp)
-    
-    def verify_alignment(self) -> bool:
-        with open(self.LTP_aligned_fp) as f:
-            seq_len = 0 # Get seq len on first pass
-            for desc, seq in self._parse_fasta(f):
-                if len(desc.strip()) < 2:
-                    logging.error("Annotation line empty")
-                    return False
-                acceptable_chars = set(["A", "C", "G", "T", "-"])
-                if seq_len == 0:
-                    seq_len = len(seq)
-                elif seq_len != len(seq):
-                    logging.error(f"Ragged alignment of length {len(seq)} instead of {seq_len}: {desc}")
-                    return False
-                if not set(list(seq)).issubset(acceptable_chars):
-                    logging.error(f"{set(list(seq))} is not a subset of {acceptable_chars}")
-                    return False
-            return True
-    
-    def clean_tree(self):
-        logging.info("Cleaning LTP tree...")
-        temp_fp = self.root_fp / "temp_tree.ntree"
-        with open(temp_fp, "w") as f_temp, open(self.LTP_tree_fp) as f_tree:
-            header = False
-            for line in f_tree.readlines():
-                if line[0] == "[":
-                    header = True
-                if header:
-                    if line[0] == "]":
-                        header = False
-                elif "'" in line and "," in line:
-                    new_id = line[line.index("'")+1:line.index(",", line.index("'"))]
-                    f_temp.write(line[:line.index("'")] + new_id + line[line.index("'", line.index("'")+1)+1:])
-                elif "'" in line:
-                    f_temp.write(line.replace("'", "").replace(" ", ""))
-                else:
-                    f_temp.write(line)
-
-        os.remove(self.LTP_tree_fp)
-        os.rename(temp_fp, self.LTP_tree_fp)
-    
-    def verify_tree(self) -> bool:
-        try:
-            with open(self.LTP_tree_fp) as f:
-                Tree("".join([l.strip() for l in f.readlines()]))
-        except NewickError as e:
-            logging.error("Error reading tree into ete3")
-            return False
-        
-        return True
-    
     
 
 
